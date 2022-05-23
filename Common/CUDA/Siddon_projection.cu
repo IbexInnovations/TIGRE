dc197ae06a68398ebb36c492085417522e99c940
cschreib-ibex <c.schreiber@ibexinnovations.co.uk>
2022-05-23 08:59:12 +0100

    Fixed missing minus sign

--- Common/CUDA/Siddon_projection.cu
+++ Common/CUDA/Siddon_projection.cu
@@ -672,7 +672,7 @@ void computeDeltas_Siddon(Geometry geo,int i, Point3D* uvorigin, Point3D* deltaU
     //End point
     Point3D P,Pu0,Pv0;
     
-    P.x  =-(geo.DSD[i]-geo.DSO[i]);   P.y  = geo.dDetecU*(((float)geo.nDetecU/2)+0.5);       P.z  = geo.dDetecV*(((float)geo.nDetecV/2)-0.5);
+    P.x  =-(geo.DSD[i]-geo.DSO[i]);   P.y  = geo.dDetecU*(-((float)geo.nDetecU/2)+0.5);       P.z  = geo.dDetecV*(((float)geo.nDetecV/2)-0.5);
     Pu0.x=0;                          Pu0.y= geo.dDetecU;                                    Pu0.z= 0;
     Pv0.x=0;                          Pv0.y= 0;                                              Pv0.z= geo.dDetecV*(-1);
 
